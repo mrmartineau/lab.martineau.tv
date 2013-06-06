88748b3a4443d5c18563745ba9e49ce17cc1ed4f
Zander Martineau <zander@martineau.tv>
2013-06-06 21:26:38 +0200

    Add check to see if input is correct

--- cloze-generator/v2/js/script.js
+++ cloze-generator/v2/js/script.js
@@ -1,58 +1,68 @@
 //Randomise plugin
 (function($) {
+	$.fn.randomize = function(childElem) {
+		return this.each(function() {
+			var $this = $(this);
+			var elems = $this.children(childElem);
 
-$.fn.randomize = function(childElem) {
-  return this.each(function() {
-      var $this = $(this);
-      var elems = $this.children(childElem);
+			elems.sort(function() { return (Math.round(Math.random())-0.5); });
 
-      elems.sort(function() { return (Math.round(Math.random())-0.5); });  
+			$this.remove(childElem);
 
-      $this.remove(childElem);  
-
-      for(var i=0; i < elems.length; i++)
-        $this.append(elems[i]);      
-
-  });    
-}
+			for(var i=0; i < elems.length; i++){
+				$this.append(elems[i]);
+			}
+		});
+	}
 })(jQuery);
 
-$(document).ready(function() {
+$(function() {
 	$('#placeholders a').click(function() {
-		var thisText =	$(this).attr('data-text');
+		var thisText = $(this).attr('data-text');
 		$('textarea').text(thisText).keyup();
 		return false;
 	});
 	//Textarea move to result box
-	$('textarea').bind('keyup',function() {        
-        $('#result_content').text($('textarea').val());
-        $('#result_content')
-        	.html('<span>'+$('#result_content')
-                .html().replace(/( |"|\(|\)|\[|\]|'|:|;|\?|\!|\/|\/\/ |, |\.)/g,'</span>$1<span>').replace(/\n/g,'</span><br /><br /><span>')+'</span>')
+	$('textarea').bind('keyup',function() {
+		$('#result_content').text($('textarea').val());
+		$('#result_content')
+			.html('<span>'+$('#result_content')
+				.html().replace(/( |"|\(|\)|\[|\]|'|:|;|\?|\!|\/|\/\/ |, |\.)/g,'</span>$1<span>').replace(/\n/g,'</span><br /><br /><span>')+'</span>')
 	});
-	
+
 	//Increment number
 	var number = 0;
 	function incrementNumber () {
-	    number += 1;
+		number += 1;
 	}
-	
+
 	//Click spans in result box
-	$('#result_content span').live('click', function(){
+	$('body').on('click', '#result_content span', function(){
 		var
-			thisTxt = $(this).text()
-			inputWidth = $(this).width();
+			thisTxt = $(this).text(),
+			inputWidth = $(this).width()
 		;
 		if ( $(this).children().size() > 0 ) {
 			//Need to match word, add a success message (tick maybe) & then replace input with correct text that is highlighted.
 		} else {
-		    incrementNumber();
-			$(this).attr({id: 'item' + number}).addClass('selected').clone().text($(this).text() + ' ').appendTo('#keyword_content');
-			$(this).html('<input type="text" placeholder="?"/>').find('input').attr({name: thisTxt}).css('width', inputWidth).data('data-text', thisTxt);
+			incrementNumber();
+			$(this).attr({id: 'item' + number})
+				.addClass('selected')
+				.clone()
+				.text($(this).text() + ' ')
+				.appendTo('#keyword_content')
+			;
+
+			$(this).html('<input type="text" placeholder="?"/>')
+				.find('input')
+				.attr({name: thisTxt})
+				.css('width', inputWidth)
+				.data('text', thisTxt)
+			;
 		}
 	});
 	//Move words back to result box
-	$('#keyword_content span').live('click', function() {
+	$('body').on('click','#keyword_content span', function() {
 		var thisClass = $(this).attr('id');
 		var thatClass = 'span#' + thisClass;
 		//alert(thatClass)
@@ -61,20 +71,34 @@ $(document).ready(function() {
 		$('#result_content').find(thatClass).replaceWith('<span>' + thisTxt + '</span>');
 		$(this).fadeOut('slow').remove();
 	});
-	
+
 	//Randomise
 	$('#keywords button').click(function() {
 		$('#keyword_content').randomize("#keyword_content span");
 	});
 	//Select text
 	/*
-$('button#select').live('click',function() {
+$('button#select').on('click',function() {
 		$('#result_content').select();
 	});
 */
-	
+
+	$('body').on('keyup', '#result_content input[type=text]', inputCheck);
+
+	function inputCheck() {
+		console.log('inputCheck');
+
+		if ( this.value === this.name ) {
+			$(this).addClass('success');
+			$(this).parent().addClass('success').text(this.name);
+			$(this).remove();
+		} else {
+			$(this).addClass('error');
+		}
+	}
+
 });
 
 // To do:
 // 1. Enter a url or paste in your own text.
-// 2. 
+// 2.
